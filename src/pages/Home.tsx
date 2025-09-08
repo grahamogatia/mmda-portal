b93b3a266a02a189e202ec074dbb68f7d07762f6
ISTI-Developer Kyle <isti@unmg.com.ph>
2025-09-08 11:33:35 +0800

    refactor: update Tabs component for improved layout and styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
-import {Button} from "../components/ui/button.tsx";
-import {Tabs, TabsContent, TabsList, TabsTrigger} from "../components/ui/tabs.tsx";
-import {locations} from "../api/locations.ts";
-import {useNavigate} from "react-router-dom"
+import { Button } from "../components/ui/button.tsx";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../components/ui/tabs.tsx";
+import { locations } from "../api/locations.ts";
+import { useNavigate } from "react-router-dom"
 import AddAdvisory from "@/components/page/AddAdvisory.tsx";
 
 
@@ -17,49 +17,25 @@ function Home() {
 
     return (
         <div className="flex flex-col items-center justify-center">
-            <header className="container border flex justify-between items-center p-4">
+            <header className="container  flex justify-between items-center p-4">
                 <p className="text-xl font-semibold">MMDA Admin Portal</p>
                 <Button type="button" onClick={onClick}>Logout</Button>
             </header>
-            <main className="border container">
-                <Tabs>
-                    <TabsList>
-                        <div>
-                            {locations.map((location) => {
-                                return (
-                                    <TabsTrigger value={location.name}>{location.name}</TabsTrigger>
-                                )
-                            })}
-                        </div>
-                        <AddAdvisory />
+            <main className="border container p-4 shadow rounded-lg space-y-4">
+                <AddAdvisory />
+                <Tabs orientation="vertical" defaultValue={locations[0].name} className="flex-row h-full">
+                    <TabsList className="flex-col h-auto gap-2">
+                        {locations.map((location) => {
+                            return (
+                                <TabsTrigger value={location.name} className="w-full">{location.name}</TabsTrigger>
+                            )
+                        })}
                     </TabsList>
-                    <TabsContent value="Tab 1">
-                        This is Tab 1
+                  {locations.map(location => {
+                    return <TabsContent key={location.id} value={location.name} className="border rounded-lg p-2">
+                        {location.name}
                     </TabsContent>
-                    <TabsContent value="Tab 2">
-
-                    </TabsContent>
-                    <TabsContent value="Tab 3">
-
-                    </TabsContent>
-                    <TabsContent value="Tab 4">
-
-                    </TabsContent>
-                    <TabsContent value="Tab 5">
-
-                    </TabsContent>
-                    <TabsContent value="Tab 6">
-
-                    </TabsContent>
-                    <TabsContent value="Tab 7">
-
-                    </TabsContent>
-                    <TabsContent value="Tab 8">
-
-                    </TabsContent>
-                    <TabsContent value="Tab 9">
-
-                    </TabsContent>
+                  })}
                 </Tabs>
             </main>
         </div>
