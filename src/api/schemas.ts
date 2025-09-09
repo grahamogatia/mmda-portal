6bf2cdb5f0a354a3575a50c1ac9b090830901b63
ISTI-Developer Kyle <isti@unmg.com.ph>
2025-09-09 11:06:53 +0800

    fix: Add code key to LocationSchema

--- src/api/schemas.ts
+++ src/api/schemas.ts
@@ -1,16 +1,19 @@
 import z from "zod";
 
 export const loginSchema = z.object({
-    username: z.string().min(4, "Username must not be empty.").max(100),
-    password: z.string().min(4, "Password must not be empty.").max(100),
+  username: z.string().min(4, "Username must not be empty.").max(100),
+  password: z.string().min(4, "Password must not be empty.").max(100),
 });
 
 const LocationSchema = z.object({
-    id:z.number(),
-    name: z.string(),
-    address: z.string(),
-})
+  id: z.number(),
+  name: z.string(),
+  address: z.string(),
+  code: z.string(),
+});
 export const advisorySchema = z.object({
-    content: z.string().min(1, "Advisory content must not be empty."),
-    location: z.array(LocationSchema).min(1, "At least one location must be selected."),
+  content: z.string().min(1, "Advisory content must not be empty."),
+  location: z
+    .array(LocationSchema)
+    .min(1, "At least one location must be selected."),
 });
